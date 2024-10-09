3a58f2cc0f4f4364c3508c49ca62dc6cb2381168
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-10-08 21:54:49 -0300

    feat: add registration methods

--- server/src/controllers/auth/userController.ts
+++ server/src/controllers/auth/userController.ts
@@ -27,3 +27,117 @@ interface TokenDocument extends Document {
   createdAt: Date;
   expiresAt: Date;
 }
+
+
+
+// Extend the Express Request type to include the user property
+declare global {
+  namespace Express {
+    interface Request {
+      user?: UserDocument;
+    }
+  }
+}
+
+export const registerUser = asyncHandler(async (req: Request, res: Response) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400).json({ message: "All fields are required" });
+    return;
+  }
+
+  if (password.length < 6) {
+    res.status(400).json({ message: "Password must be at least 6 characters" });
+    return;
+  }
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400).json({ message: "User already exists" });
+    return;
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  const token = generateToken(user._id.toString());
+
+  res.cookie("token", token, {
+    path: "/",
+    httpOnly: true,
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    sameSite: "none",
+    secure: true,
+  });
+
+  if (user) {
+    const { _id, name, email, role, photo, bio, isVerified } = user;
+    res.status(201).json({
+      _id,
+      name,
+      email,
+      role,
+      photo,
+      bio,
+      isVerified,
+      token,
+    });
+  } else {
+    res.status(400).json({ message: "Invalid user data" });
+  }
+});
+
+export const loginUser = asyncHandler(async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400).json({ message: "All fields are required" });
+    return;
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    res.status(404).json({ message: "User not found, sign up!" });
+    return;
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    res.status(400).json({ message: "Invalid credentials" });
+    return;
+  }
+
+  const token = generateToken(user._id.toString());
+
+  if (user && isMatch) {
+    const { _id, name, email, role, photo, bio, isVerified } = user;
+
+    res.cookie("token", token, {
+      path: "/",
+      httpOnly: true,
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      sameSite: "none",
+      secure: true,
+    });
+
+    res.status(200).json({
+      _id,
+      name,
+      email,
+      role,
+      photo,
+      bio,
+      isVerified,
+      token,
+    });
+  } else {
+    res.status(400).json({ message: "Invalid email or password" });
+  }
+});
