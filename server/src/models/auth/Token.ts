356ceac0a16adad621376a73d127dc9f21f21438
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-10-08 21:17:50 -0300

    feat: add token model

--- server/src/models/auth/Token.ts
+++ server/src/models/auth/Token.ts
@@ -8,3 +8,41 @@ interface IToken extends Document {
   createdAt: Date;
   expiresAt: Date;
 }
+
+// Create the Token schema
+const TokenSchema: Schema = new mongoose.Schema({
+  // Reference to the user this token belongs to
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+
+ 
+  verificationToken: {
+    type: String,
+    default: "",
+  },
+
+  
+  passwordResetToken: {
+    type: String,
+    default: "",
+  },
+
+  createdAt: {
+    type: Date,
+    required: true,
+  },
+
+  
+  expiresAt: {
+    type: Date,
+    required: true,
+  },
+});
+
+
+const Token = mongoose.model<IToken>("Token", TokenSchema);
+
+export default Token;
