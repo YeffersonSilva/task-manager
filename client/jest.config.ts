db8ae0b5b2c72ec372f25b13388e2afc02ff5135
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-10-10 19:59:52 -0300

    chore: configure testing setup

---
+++ client/jest.config.ts
@@ -0,0 +1,8 @@
+export default {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  transformIgnorePatterns: ['node_modules/(?!axios)'],
+};
